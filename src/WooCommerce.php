8a7004bd31044232df9009b4ab67144b91f6473a
Oskar Schöldström <m@oxy.fi>
2020-04-24 11:00:40 -0300

    refactor

---
+++ src/WooCommerce.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Genero\Sage\WooCommerce;
+
+use Illuminate\Contracts\Container\Container as ContainerContract;
+use Roots\Acorn\Sage\Sage;
+use Roots\Acorn\Sage\ViewFinder;
+use Roots\Acorn\View\FileViewFinder;
+use Illuminate\Support\Str;
+
+use function Roots\view;
+
+class WooCommerce
+{
+    public function __construct(
+        ViewFinder $sageFinder,
+        FileViewFinder $fileFinder,
+        Sage $sage,
+        ContainerContract $app
+    ) {
+        $this->app = $app;
+        $this->fileFinder = $fileFinder;
+        $this->sageFinder = $sageFinder;
+        $this->sage = $sage;
+    }
+
+    /**
+     * Load template hook overrides file if available in app/ folder of theme.
+     */
+    public function loadThemeTemplateHooks()
+    {
+        locate_template('app/wc-template-hooks.php', true, true);
+    }
+
+    /**
+     * Declare theme support.
+     */
+    public function addThemeSupport(): void
+    {
+        add_theme_support('woocommerce');
+    }
+
+    /**
+     * Support blade templates for the main template include.
+     */
+    public function templateInclude(string $template): string
+    {
+        if (strpos($template, \WC_ABSPATH) === -1) {
+            return $template;
+        }
+        return $this->locateThemeTemplate($template) ?: $template;
+    }
+
+    /**
+     * Support blade templates for the woocommerce comments/reviews.
+     */
+    public function reviewsTemplate(string $template): string
+    {
+        // Unless it's a WC template, keep using the Sage's default filter.
+        if (strpos($template, \WC_ABSPATH) === -1) {
+            return $this->sage->filterCommentsTemplate($template);
+        }
+
+        return $this->template($template);
+    }
+
+    /**
+     * Filter a template path, taking into account theme templates and creating
+     * blade loaders as needed.
+     */
+    public function template(string $template): string
+    {
+        // Locate any matching template within the theme.
+        $themeTemplate = $this->locateThemeTemplate($template);
+        if (!$themeTemplate) {
+            return $template;
+        }
+
+        // Include directly unless it's a blade file.
+        if (!Str::endsWith($themeTemplate, '.blade.php')) {
+            return $themeTemplate;
+        }
+
+        // We have a template, create a loader file and return it's path.
+        return view(
+            $this->fileFinder->getPossibleViewNameFromPath($themeTemplate)
+        )->makeLoader();
+    }
+
+    /**
+     * Locate the theme's woocommerce blade template when available.
+     */
+    protected function locateThemeTemplate(string $template): string
+    {
+        $themeTemplate = WC()->template_path() . str_replace(\WC_ABSPATH . 'templates/', '', $template);
+        return locate_template($this->sageFinder->locate($themeTemplate));
+    }
+}
--- src/WooCommerceServiceProvider.php
+++ src/WooCommerceServiceProvider.php
@@ -3,9 +3,6 @@
 namespace Genero\Sage\WooCommerce;
 
 use Roots\Acorn\ServiceProvider;
-use Illuminate\Support\Str;
-
-use function Roots\view;
 
 class WooCommerceServiceProvider extends ServiceProvider
 {
@@ -16,9 +13,7 @@ class WooCommerceServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->viewFinder = $this->app['view.finder'];
-        $this->sageFinder = $this->app['sage.finder'];
-        $this->sage = $this->app['sage'];
+        $this->app->singleton('woocommerce', WooCommerce::class);
     }
 
     /**
@@ -28,88 +23,29 @@ class WooCommerceServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!defined('WC_ABSPATH')) {
-            return;
+        if (defined('WC_ABSPATH')) {
+            $this->app['woocommerce']->loadThemeTemplateHooks();
+            $this->bindSetupAction();
+            $this->bindFilters();
         }
+    }
 
-        // Load the template hook overrides if available.
-        locate_template('app/wc-template-hooks.php', true, true);
+    public function bindFilters()
+    {
+        $woocommerce = $this->app['woocommerce'];
+        $sage = $this->app['sage'];
+
+        add_filter('template_include', [$woocommerce, 'templateInclude'], 11);
+        add_filter('woocommerce_locate_template', [$woocommerce, 'template']);
+        add_filter('wc_get_template_part', [$woocommerce, 'template']);
 
         // Unhook Sage's filter and run our filter after WooCommerce.
-        remove_filter('comments_template', [$this->sage, 'filterCommentsTemplate']);
-        add_filter('comments_template', [$this, 'reviewsTemplate'], 11);
-
-        add_action('after_setup_theme', [$this, 'addThemeSupport']);
-        add_filter('template_include', [$this, 'templateInclude'], 11);
-        add_filter('woocommerce_locate_template', [$this, 'template']);
-        add_filter('wc_get_template_part', [$this, 'template']);
+        remove_filter('comments_template', [$sage, 'filterCommentsTemplate']);
+        add_filter('comments_template', [$woocommerce, 'reviewsTemplate'], 11);
     }
 
-    public function applyThemeModifications(): void
+    public function bindSetupAction()
     {
-    }
-
-    /**
-     * Declare theme support.
-     */
-    public function addThemeSupport(): void
-    {
-        add_theme_support('woocommerce');
-    }
-
-    /**
-     * Support blade templates for the main template include.
-     */
-    public function templateInclude(string $template): string
-    {
-        if (strpos($template, \WC_ABSPATH) === -1) {
-            return $template;
-        }
-        return $this->locateThemeTemplate($template) ?: $template;
-    }
-
-    /**
-     * Support blade templates for the woocommerce comments/reviews.
-     */
-    public function reviewsTemplate(string $template): string
-    {
-        // Unless it's a WC template, keep using the Sage's default filter.
-        if (strpos($template, \WC_ABSPATH) === -1) {
-            return $this->sage->filterCommentsTemplate($template);
-        }
-
-        return $this->template($template);
-    }
-
-    /**
-     * Filter a template path, taking into account theme templates and creating
-     * blade loaders as needed.
-     */
-    public function template(string $template): string
-    {
-        // Locate any matching template within the theme.
-        $themeTemplate = $this->locateThemeTemplate($template);
-        if (!$themeTemplate) {
-            return $template;
-        }
-
-        // Include directly unless it's a blade file.
-        if (!Str::endsWith($themeTemplate, '.blade.php')) {
-            return $themeTemplate;
-        }
-
-        // We have a template, create a loader file and return it's path.
-        return view(
-            $this->viewFinder->getPossibleViewNameFromPath($themeTemplate)
-        )->makeLoader();
-    }
-
-    /**
-     * Locate the theme's woocommerce blade template when available.
-     */
-    protected function locateThemeTemplate(string $template): string
-    {
-        $themeTemplate = WC()->template_path() . str_replace(\WC_ABSPATH . 'templates/', '', $template);
-        return locate_template($this->sageFinder->locate($themeTemplate));
+        add_action('after_setup_theme', [$this->app['woocommerce'], 'addThemeSupport']);
     }
 }
