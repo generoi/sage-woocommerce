01041caec9bda5bb1fcaa6f763648662b9fb8d26
Oskar Schöldström <m@oxy.fi>
2026-05-22 12:45:15 -0300

    Fix published hook stub line length

--- publishes/app/wc-template-hooks.php
+++ publishes/app/wc-template-hooks.php
@@ -224,7 +224,7 @@
  * Cart widget
  */
 // remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_button_view_cart', 10);
-// remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout', 20);
+// remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout',20);
 // remove_action('woocommerce_widget_shopping_cart_total', 'woocommerce_widget_shopping_cart_subtotal', 10);
 
 /**
